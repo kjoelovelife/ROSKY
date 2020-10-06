d6edee6986773932cdfc1754ea38a8f28041cdb9
Joe Lin <kjoelovelife@gmail.com>
2020-10-06 09:00:38 +0800

    add lane_following.launch

--- install_script/catkin_make.sh
+++ install_script/catkin_make.sh
@@ -8,7 +8,7 @@ cd ~/$main_path/$cvbridge_path
 catkin config -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/aarch64-linux-gnu/libpython3.6m.so
 catkin config --install
 python3-config --includes
-catkin build cv_bridge
+catkin build
 source ~/$main_path/$cvbridge_path/install/setup.bash --extend
 
 
