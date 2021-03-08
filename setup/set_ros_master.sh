f9d2bc604e9e1748b792c4a78d181a6b165e95c5
Joe Lin <kjoelovelife@gmail.com>
2021-03-08 10:45:01 +0800

    add ROS_HOSTNAME in set_ros_master.sh

--- setup/set_ros_master.sh
+++ setup/set_ros_master.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
+current_ip=$(hostname -I | awk '{print $1}')
+hardware=$(uname -i)
+echo "========================================================"
 echo "Setting ROS_MASTER_URI..."
 if [ $# -gt 0 ]; then
-	# provided a hostname, use it as ROS_MASTER_URI
-	export ROS_MASTER_URI=http://$1.local:11311/
+    # provided a hostname, use it as ROS_MASTER_URI
+    export ROS_MASTER_URI=http://$1.local:11311/
 else
-	echo "No hostname provided. Using $HOSTNAME."
-	export ROS_MASTER_URI=http://$HOSTNAME.local:11311/
+    echo "No hostname provided. Using $HOSTNAME."
+    export ROS_MASTER_URI=http://$HOSTNAME.local:11311/
 fi
 
 echo "ROS_MASTER_URI set to $ROS_MASTER_URI"
+echo ""
+
+#==== set ROS_HOSTNAME ====
+if [ "$hardware" "==" "aarch64" ]; then
+    echo "You may use ROSKY on Jetson nano. If not, please checkout the ROS_HOSTNAME is right."
+else
+    echo "Setting ROS_HOSTNAME..."
+    export ROS_HOSTNAME=$current_ip
+    echo "ROS_HOSTNAME set to $current_ip"
+fi
+
+echo "========================================================"
+
+
+
