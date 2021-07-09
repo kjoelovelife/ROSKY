109b129e3e6f399c8fca86e002e3d6ca1fbea3ea
Lin Wei-Chih <34365220+kjoelovelife@users.noreply.github.com>
2021-07-09 13:48:42 +0800

    Update ros_install_melodic.sh
    change apt-key for melodic in line 16

--- install_script/ros_install_melodic.sh
+++ install_script/ros_install_melodic.sh
@@ -12,8 +12,9 @@ if [ "$ros_install" '==' "y" ] || [ "$ros_install" '==' "Y" ]; then
     echo $PASSWORD | sudo -S apt-add-repository multiverse
     echo $PASSWORD | sudo -S apt-add-repository restricted
     # add ROS repository to apt sources
+    sudo apt update && sudo apt install -y curl gnupg2 lsb-release
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
     ## delete ROS old key : sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116 
     # install ROS desktop-full
     sudo apt-get update
