488c47d3ad76c57b455a83ec93a22a431d7af4f7
Lin Wei-Chih <34365220+kjoelovelife@users.noreply.github.com>
2021-07-26 11:04:49 +0800

    Update ros_install_melodic.sh

--- install_script/ros_install_melodic.sh
+++ install_script/ros_install_melodic.sh
@@ -13,13 +13,14 @@ if [ "$ros_install" '==' "y" ] || [ "$ros_install" '==' "Y" ]; then
     echo $PASSWORD | sudo -S apt-add-repository restricted
     # add ROS repository to apt sources
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+    sudo apt -y install curl # if you haven't already installed curl
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
     ## delete ROS old key : sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116 
     # install ROS desktop-full
     sudo apt-get update
     sudo apt install -y ros-$ros1_distro-desktop-full
-    sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
-    sudo apt install python-rosdep
+    sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+    sudo apt install -y python-rosdep
     sudo rosdep init
     rosdep update
     sudo apt-get install -y python-rosinstall \
