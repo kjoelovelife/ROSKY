9905e88676b32c988df2d330ea32908e4f62fcbf
Lin Wei-Chih <34365220+kjoelovelife@users.noreply.github.com>
2021-07-28 13:59:22 +0800

    Update set_ros_master.sh

--- setup/set_ros_master.sh
+++ setup/set_ros_master.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 current_ip=$(hostname -I | awk '{print $1}')
 hardware=$(uname -r)
-echo "========================================================"
+echo "======== Setup ROS ========"
 echo "Setting ROS_MASTER_URI..."
 if [ $# -gt 0 ]; then
     # provided a hostname, use it as ROS_MASTER_URI
@@ -19,4 +19,4 @@ echo "Setting ROS_HOSTNAME..."
 export ROS_HOSTNAME=$current_ip
 echo "ROS_HOSTNAME set to $current_ip"
 
-echo "========================================================"
+echo "==== End ===="
